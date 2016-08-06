6fa50d6be998ab2876483f23d7ddf9bb0b0a58ac
Tim <timothy.r.koski@gmail.com>
2016-08-06 17:31:43 -0400

    Learning Console & Hierarchy
    Learning how to use the Console and Hierarchy windows

---
+++ EditorInterface/Assets/LogMessages.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LogMessages : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Hello World");
+        Debug.LogWarning("I am a warning!");
+        Debug.LogError("!!!I AM AN ERROR!!!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

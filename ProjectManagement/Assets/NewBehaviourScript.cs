02fb69898469e4936aac80a38df69a181879b898
timothy-r-koski <timothy.r.koski@gmail.com>
2016-08-15 21:23:52 -0400

    Learned about tags

---
+++ ProjectManagement/Assets/NewBehaviourScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+
+    // Use this for initialization
+    void Start () {
+       var cube  = GameObject.FindWithTag("Cube");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+      
+	}
+}

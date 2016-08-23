c0a64de679ac5830373bdf3649a6f0e901a7ac2a
Tim <timothy.r.koski@gmail.com>
2016-08-22 21:25:12 -0400

    Learned about variables

---
+++ Programming/Assets/AverageCalc.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AverageCalc : MonoBehaviour {
+
+    public float inputOne;
+    public float inputTwo;
+
+	// Use this for initialization
+	void Start () {
+        print("Average of " + inputOne + " and " + inputTwo + " = " + Average(inputOne, inputTwo));
+	}
+
+    public float Average(float numberOne, float numberTwo) 
+    {
+        return (numberOne + numberTwo) / 2;
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Programming/Assets/ConsoleWelcomer.cs
+++ Programming/Assets/ConsoleWelcomer.cs
@@ -1,17 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ConsoleWelcomer : MonoBehaviour {
+public class ConsoleWelcomer : MonoBehaviour
+{
 
-    
+    public float accelDueToGrav = 9.41f;
 
-    
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         int anInteger = 1000;
-    float someFloat = 2.5f;
-    float sum = anInteger + someFloat;
-    float mean = sum / 2;
+        float someFloat = 2.5f;
+        float sum = anInteger + someFloat;
+        float mean = sum / 2;
 
         print("Hello my name is Tim Koski");
         print("Welcome to my program");
@@ -22,18 +23,29 @@ public class ConsoleWelcomer : MonoBehaviour {
         print("sum = " + sum);
         print("mean = " + Average(sum, 2));
         print("square = " + Square(someFloat));
-	}
 
-    float Square(float numToSqr) {
+        print("Acceleration due to gravity = " + accelDueToGrav);
+        print("Force on a 10kg mass due to gravity = " + ForceDueToGravity(10));
+    }
+
+    float ForceDueToGravity(float mass)
+    {
+        return accelDueToGrav * mass;
+    }
+
+    float Square(float numToSqr)
+    {
         return numToSqr + numToSqr;
     }
 
-    float Average(float sum, float numOfNumbers) {
+    float Average(float sum, float numOfNumbers)
+    {
         return sum / numOfNumbers;
     }
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }

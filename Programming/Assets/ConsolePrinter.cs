914f0cdd52be1d69076385cb00b380d822b3d0a7
Tim <timothy.r.koski@gmail.com>
2016-08-22 21:43:26 -0400

    Learned about Vector3

--- Programming/Assets/ConsolePrinter.cs
+++ Programming/Assets/ConsolePrinter.cs
@@ -3,10 +3,15 @@ using System.Collections;
 
 public class ConsolePrinter : MonoBehaviour
 {
+    public Vector3 currentLocation;
 
 	// Use this for initialization
 	void Start () {
-        print("Hello World");
+        Vector3 home = new Vector3(0.0f, 0.0f, 0.0f);
+        Vector3 distanceToHome = currentLocation - home;
+        print("current location = " + currentLocation);
+        print("home location = " + home);
+        print("distance from home:" + distanceToHome.magnitude);
 	}
 	
 	// Update is called once per frame

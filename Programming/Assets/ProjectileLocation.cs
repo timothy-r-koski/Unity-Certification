16381df198d1fbc96134dc364bbbd1e53b73b81c
Tim <timothy.r.koski@gmail.com>
2016-08-22 22:08:04 -0400

    Learned about Time.deltaTime

---
+++ Programming/Assets/ProjectileLocation.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileLocation : MonoBehaviour {
+
+    public float speed = 5f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+    public void UpdatePosition() {
+        Transform location = GetComponent<Transform>();
+        location.position = location.position + new Vector3(0.0f, 0.0f, (speed * Time.deltaTime));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Programming/Assets/ProjectileMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileMovement : MonoBehaviour {
+
+    ProjectileLocation projectile;
+
+    // Use this for initialization
+    void Start () {
+        projectile = GetComponent<ProjectileLocation>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        projectile.UpdatePosition();
+    }
+}
